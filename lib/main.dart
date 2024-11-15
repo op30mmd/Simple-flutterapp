082954f0629a3f48c3b40a0560a17f47e394052e
op30mmd <152014555+op30mmd@users.noreply.github.com>
2024-11-15 18:37:02 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  static const String _title = 'Flutter Stateful Clicker Counter';
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: _title,
+      title: 'HTTP Request App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
-      home: const MyHomePage(),
+      home: HttpRequestPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
+class HttpRequestPage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HttpRequestPageState createState() => _HttpRequestPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HttpRequestPageState extends State<HttpRequestPage> {
+  final TextEditingController _urlController = TextEditingController();
+  String _response = '';
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  Future<void> _makeRequest(String method) async {
+    try {
+      final url = Uri.parse(_urlController.text);
+      http.Response response;
+
+      switch (method) {
+        case 'GET':
+          response = await http.get(url);
+          break;
+        case 'POST':
+          response = await http.post(url, body: {'key': 'value'});
+          break;
+        case 'PUT':
+          response = await http.put(url, body: {'key': 'updated value'});
+          break;
+        case 'DELETE':
+          response = await http.delete(url);
+          break;
+        default:
+          throw 'Unsupported HTTP method';
+      }
+
+      setState(() {
+        _response = 'Status Code: ${response.statusCode}\n\nBody:\n${response.body}';
+      });
+    } catch (e) {
+      setState(() {
+        _response = 'Error: $e';
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter Demo Click Counter'),
+        title: Text('HTTP Request App'),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            TextField(
+              controller: _urlController,
+              decoration: InputDecoration(
+                labelText: 'Enter URL',
+                border: OutlineInputBorder(),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: const TextStyle(fontSize: 25),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () => _makeRequest('GET'),
+                  child: Text('GET'),
+                ),
+                ElevatedButton(
+                  onPressed: () => _makeRequest('POST'),
+                  child: Text('POST'),
+                ),
+                ElevatedButton(
+                  onPressed: () => _makeRequest('PUT'),
+                  child: Text('PUT'),
+                ),
+                ElevatedButton(
+                  onPressed: () => _makeRequest('DELETE'),
+                  child: Text('DELETE'),
+                ),
+              ],
+            ),
+            SizedBox(height: 16),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Text(
+                  _response,
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
