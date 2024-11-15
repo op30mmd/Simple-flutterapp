0be0974eb8d18a646894cf5777920d0a69a48704
op30mmd <152014555+op30mmd@users.noreply.github.com>
2024-11-15 12:55:07 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,66 @@
-// Importing important packages require to connect
-// Flutter and Dart
 import 'package:flutter/material.dart';
 
-// Main Function
 void main() {
-// Giving command to runApp() to run the app.
-
-/* The purpose of the runApp() function is to attach
-the given widget to the screen. */
-runApp(const MyApp());
+  runApp(const MyApp());
 }
 
-// Widget is used to create UI in flutter framework.
-
-/* StatelessWidget is a widget, which does not maintain
-any state of the widget. */
-
-/* MyApp extends StatelessWidget and overrides its
-build method. */
 class MyApp extends StatelessWidget {
-const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
-// This widget is the root of your application.
-@override
-Widget build(BuildContext context) {
-	return MaterialApp(
-	// title of the application
-	title: 'Hello World Demo Application',
-	// theme of the widget
-	theme: ThemeData(
-		primarySwatch: Colors.lightGreen,
-	),
-	// Inner UI of the application
-	home: const MyHomePage(title: 'Home page'),
-	);
-}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello World Demo Application',
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+      ),
+      home: const MyHomePage(title: 'Home page'),
+    );
+  }
 }
 
-/* This class is similar to MyApp instead it
-returns Scaffold Widget */
-class MyHomePage extends StatelessWidget {
-const MyHomePage({Key? key, required this.title}) : super(key: key);
-final String title;
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
 
-@override
-Widget build(BuildContext context) {
-	return Scaffold(
-	appBar: AppBar(
-		title: Text(title),
-	),
-	// Sets the content to the
-	// center of the application page
-	body: const Center(
-		// Sets the content of the Application
-		child: Text(
-		'Welcome to GeeksForGeeks!',
-	)),
-	);
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
 }
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
 }
